8d28abe37fbd293cea36585996761027d952a715
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-23 14:15:26 +1000

    Building custom tokencredential

--- src/main/java/com/matthewcasperson/azureapi/controllers/UploadFileController.java
+++ src/main/java/com/matthewcasperson/azureapi/controllers/UploadFileController.java
@@ -1,6 +1,8 @@
 package com.matthewcasperson.azureapi.controllers;
 
+import com.azure.core.credential.AccessToken;
 import com.azure.core.credential.TokenCredential;
+import com.azure.core.credential.TokenRequestContext;
 import com.azure.core.util.BinaryData;
 import com.azure.identity.AuthorizationCodeCredential;
 import com.azure.storage.blob.BlobClient;
@@ -14,6 +16,10 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import javax.persistence.Access;
+import java.time.ZoneOffset;
 
 @RestController
 public class UploadFileController {
@@ -23,7 +29,15 @@ public class UploadFileController {
                        @AuthenticationPrincipal OidcUser principal,
                        @RegisteredOAuth2AuthorizedClient("storage") OAuth2AuthorizedClient client) {
 
-        TokenCredential tokenCredential = new AuthorizationCodeCredential(client.getAccessToken().toString());
+        TokenCredential tokenCredential = new TokenCredential() {
+            @Override
+            public Mono<AccessToken> getToken(TokenRequestContext request) {
+                return Mono.just(new AccessToken(
+                            client.getAccessToken().getTokenValue(),
+                            client.getAccessToken().getExpiresAt().atOffset(ZoneOffset.UTC))
+                );
+            }
+        };
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().credential(tokenCredential).buildClient();
         BlobContainerClient containerClient = blobServiceClient.createBlobContainer(principal.getEmail());
         BlobClient blobClient = containerClient.getBlobClient(fileName);
