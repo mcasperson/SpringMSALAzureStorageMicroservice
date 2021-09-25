96bf39e60709b1cfa93f966c106ad3e7fd55b7c1
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-25 19:30:25 +1000

    Added audit log

---
+++ src/main/java/com/matthewcasperson/azureapi/controllers/AuditController.java
@@ -0,0 +1,23 @@
+// src/main/java/com/matthewcasperson/demo/controllers/UploadController.java
+
+package com.matthewcasperson.azureapi.controllers;
+
+import com.matthewcasperson.azureapi.model.Audit;
+import com.matthewcasperson.azureapi.repository.AuditRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AuditController {
+
+    @Autowired
+    AuditRepository auditRepository;
+
+    @GetMapping(value = "/audit", produces = "application/json")
+    public List<Audit> upload() {
+        return auditRepository.findAll();
+    }
+}
